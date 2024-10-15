531acbbf047312d75ca72b625890eee669fff981
Shades-of-Dark <raovedh@gmail.com>
2024-10-14 22:06:29 -0700

    need schoology auth token

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import requests
+from authomatic import Authomatic
+import flask
+import oauthlib
+from bs4 import BeautifulSoup
 
 
+response = requests.get("https://app.schoology.com/login")
+app = flask.Flask(__name__)
 
+#response.raise_for_status()
+
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
+@app.route('/')
+def home():
+    return "Welcome to my home. We appear to be lacking in <i> furniture <i>."
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
