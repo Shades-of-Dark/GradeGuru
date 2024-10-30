06196769152ebeaa1d59b23344485685ce1b909c
Shades-of-Dark <raovedh@gmail.com>
2024-10-29 22:26:12 -0700

    just a few sthawdishjf

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
 from authomatic import Authomatic
-import flask
+from flask import Flask, render_template
 import oauthlib
 from bs4 import BeautifulSoup
 
 
 #response = requests.get("https://app.schoology.com/login")
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 #response.raise_for_status()
 
 #soup = BeautifulSoup(response.content, 'html.parser')
 #print(soup)
+
+
 @app.route('/')
 def home():
     return "Welcome to my home. We appear to be lacking in <i> furniture <i>."
